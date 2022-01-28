a02c3c98fbf5002b22f725766ff399e76823080e
hendrychjan <jan.honza.hendrych@gmail.com>
2022-01-28 10:50:05 +0100

    Implemented composable graphs

--- models/collectionSettings.js
+++ models/collectionSettings.js
@@ -6,7 +6,7 @@ const collectionSettingsSchema = new mongoose.Schema({
     type: mongoose.Schema.Types.ObjectId,
     ref: "Collection",
   },
-  barGraph: {
+  composedGraph: {
     xAxis: {
       dataKey: { type: String, default: "" },
       type: { type: String, default: "category" },
@@ -52,6 +52,42 @@ const collectionSettingsSchema = new mongoose.Schema({
       ],
       default: [],
     },
+    lines: {
+      type: [
+        {
+          dataKey: { type: String, default: "" },
+          lineType: { type: String, default: "linear" },
+          stroke: { type: String, default: "#7bed55" },
+          dot: { type: Boolean, default: true },
+          activeDot: { type: Boolean, default: true },
+          label: { type: Boolean, default: false },
+          strokeWidth: { type: Number, default: 1 },
+          connectNulls: { type: Boolean, default: false },
+          unit: { type: String, default: "" },
+          name: { type: String, default: "" },
+          hide: { type: Boolean, default: false },
+        },
+      ],
+      default: [],
+    },
+    areas: {
+      type: [
+        {
+          dataKey: { type: String, default: "" },
+          lineType: { type: String, default: "linear" },
+          stroke: { type: String, default: "#7bed55" },
+          dot: { type: Boolean, default: false },
+          activeDot: { type: Boolean, default: false },
+          label: { type: Boolean, default: false },
+          connectNulls: { type: Boolean, default: false },
+          unit: { type: String, default: "" },
+          name: { type: String, default: "" },
+          stackId: { type: String, default: "" },
+          hide: { type: Boolean, default: false },
+        },
+      ],
+      default: [],
+    },
   },
 });
 
@@ -84,16 +120,50 @@ function validateCollectionSettings(payload) {
     hide: Joi.boolean().required(),
   });
 
+  const lineSchema = Joi.object({
+    _id: Joi.any(),
+    __v: Joi.any(),
+    dataKey: Joi.string().allow("").required(),
+    lineType: Joi.string().allow("basis", "basisClosed", "basisOpen", "linear", "linearClosed", "natural", "monotone", "monotoneX", "monotoneY", "step", "stepBefore", "stepAfter").required(),
+    stroke: Joi.string().regex(/^#([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$/).required(),
+    dot: Joi.boolean().required(),
+    activeDot: Joi.boolean().required(),
+    label: Joi.boolean().required(),
+    strokeWidth: Joi.number().min(1).max(20).required(),
+    connectNulls: Joi.boolean().required(),
+    unit: Joi.string().allow("").required(),
+    name: Joi.string().allow("").required(),
+    hide: Joi.boolean().required(),
+  });
+
+  const areaSchema = Joi.object({
+    _id: Joi.any(),
+    __v: Joi.any(),
+    dataKey: Joi.string().allow("").required(),
+    lineType: Joi.string().allow("basis", "basisClosed", "basisOpen", "linear", "linearClosed", "natural", "monotone", "monotoneX", "monotoneY", "step", "stepBefore", "stepAfter").required(),
+    stroke: Joi.string().regex(/^#([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$/).allow("").required(),
+    dot: Joi.boolean().required(),
+    activeDot: Joi.boolean().required(),
+    label: Joi.boolean().required(),
+    connectNulls: Joi.boolean().required(),
+    unit: Joi.string().allow("").required(),
+    name: Joi.string().allow("").required(),
+    stackId: Joi.string().allow("").required(),
+    hide: Joi.boolean().required(),
+  });
+
   const schema = Joi.object({
     _id: Joi.any(),
     __v: Joi.any(),
     parent: Joi.string()
       .regex(/^[0-9a-fA-F]{24}$/)
       .required(),
-    barGraph: Joi.object({
+    composedGraph: Joi.object({
       xAxis: axisSettingsSchema.required(),
       yAxis: axisSettingsSchema.required(),
       bars: Joi.array().items(barSchema),
+      lines: Joi.array().items(lineSchema),
+      areas: Joi.array().items(areaSchema),
     }).required(),
   });
 
